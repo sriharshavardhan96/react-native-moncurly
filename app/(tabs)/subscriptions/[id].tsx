69b140a04a9cbf0bbfb5ace081c54863614be11e
shv-peri <harsha.vardhan@vation.com>
2026-04-06 15:36:24 +0530

    feat: first commit

---
+++ app/(tabs)/subscriptions/[id].tsx
@@ -0,0 +1,24 @@
+import { Link, useLocalSearchParams } from "expo-router";
+import { Text, View } from 'react-native';
+// import { usePostHog } from 'posthog-react-native';
+
+const SubscriptionDetails = () => {
+    const { id } = useLocalSearchParams<{ id: string }>();
+    // const posthog = usePostHog();
+
+  /*   useEffect(() => {
+        // Only capture if id is valid
+        if (id && typeof id === 'string' && id.trim()) {
+            posthog.capture('subscription_details_viewed', { subscription_id: id });
+        }
+    }, [id, posthog]); */
+
+    return (
+        <View>
+            <Text>Subscription Details: {id}</Text>
+            <Link href="/">Go back</Link>
+        </View>
+    )
+}
+
+export default SubscriptionDetails
