25f3bc422a8828e8114eae0c639f2a6e6f2e307a
shv-peri <harsha.vardhan@vation.com>
2026-04-07 14:34:16 +0530

    feat: added fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,18 +8,9 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="flex-1 bg-background p-5">
-          <Text className="text-xl font-bold text-success">
-            Welcome to Nativewind!
-          </Text>
-          <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary p-4 text-white">Sign In</Link>
-          <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary p-4 text-white">Sign Up</Link>
-          <Link href={{
-            pathname: "/subscriptions/[id]",
-            params: {id: 'claude-max'},
-          }} 
-          className="mt-4 rounded bg-primary p-4 text-white">
-            Claude Max Subscription
-          </Link>
+          <Text className="text-5xl font-sans-extrabold">Home </Text>
+          <Link href="/(auth)/sign-in" className="mt-4 font-sans-bold rounded bg-primary p-4 text-white">Sign In</Link>
+          <Link href="/(auth)/sign-up" className="mt-4 font-sans-bold rounded bg-primary p-4 text-white">Sign Up</Link>
     </SafeAreaView>
       
   );
